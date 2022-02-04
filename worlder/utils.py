0fa1b4dcc42f9b89853d687b2add07e69b78a9dc
mikhailkravets <mike.k@coaxsoft.com>
2022-02-04 10:26:45 +0200

    Add google style docstring

--- worlder/utils.py
+++ worlder/utils.py
@@ -3,8 +3,11 @@
 def string_multiplier(s: str, power: int = 3) -> str:
     """Return string multiplied by power
 
-    :param s: initial string
-    :param power: power to multiply
-    :return: multiplied string
+    Args:
+        s (str): initial string
+        power (int): power to multiply
+
+    Returns:
+        str: multiplied string
     """
     return s * power
